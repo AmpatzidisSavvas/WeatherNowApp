240547c3188b91fd1409804daec1e7e29beeb65e
Ampatzidis Savvas <134397286+AmpatzidisSavvas@users.noreply.github.com>
2023-08-18 10:10:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const apiKey = "372ff38be7b6ea10589cbacb87014a8a"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=372ff38be7b6ea10589cbacb87014a8a&q="
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    const response = await fetch(apiUrl + city)
 
     if (response.status == 404) {
 
@@ -14,25 +13,25 @@ async function checkWeather(city) {
 
         var data = await response.json()
 
-    $('.city').html(data.name)
-    $('.temp').html(Math.round(data.main.temp) + '°C')
-    $('.humidity').html(data.main.humidity + `%`)
-    $('.wind').html(data.wind.speed + `km/h`)
+        $('.city').html(data.name)
+        $('.temp').html(Math.round(data.main.temp) + '°C')
+        $('.humidity').html(data.main.humidity + `%`)
+        $('.wind').html(data.wind.speed + `km/h`)
 
-    if(data.weather[0].main == "Clouds") {
-        $('.weather-icon').attr('src', 'img/clouds.png')
-    } else if (data.weather[0].main == "Clear") {
-        $('.weather-icon').attr('src', 'img/clear.png')
-    } else if (data.weather[0].main == "Rain") {
-        $('.weather-icon').attr('src', 'img/rain.png')
-    } else if (data.weather[0].main == "Drizzle") {
-        $('.weather-icon').attr('src', 'img/drizzle.png')
-    }  else if (data.weather[0].main == "Mist") {
-        $('.weather-icon').attr('src', 'img/mist.png')
-    }
+        if(data.weather[0].main == "Clouds") {
+            $('.weather-icon').attr('src', 'img/clouds.png')
+        } else if (data.weather[0].main == "Clear") {
+            $('.weather-icon').attr('src', 'img/clear.png')
+        } else if (data.weather[0].main == "Rain") {
+            $('.weather-icon').attr('src', 'img/rain.png')
+        } else if (data.weather[0].main == "Drizzle") {
+            $('.weather-icon').attr('src', 'img/drizzle.png')
+        }  else if (data.weather[0].main == "Mist") {
+            $('.weather-icon').attr('src', 'img/mist.png')
+        }
 
-    $('.weather').css('display','block')
-    $('.error').css('display', 'none')
+        $('.weather').css('display','block')
+        $('.error').css('display', 'none')
 
     }
 
